0195d8f534ca94e80b065ce3cc4be83bbc19949f
Alex Harrison <alexkendall.harrison@gmail.com>
2016-06-21 13:05:47 -0400

    configured underdark dependcy

---
+++ android/app/src/main/java/com/rctunderdark/NetworkManager.java
@@ -0,0 +1,113 @@
+package com.rctunderdark;
+import org.w3c.dom.Node;
+
+import io.underdark.*;
+import io.underdark.transport.Link;
+import io.underdark.transport.Transport;
+import io.underdark.transport.TransportKind;
+import io.underdark.transport.TransportListener;
+import android.app.Activity;
+import android.os.Debug;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.*;
+
+public class NetworkManager extends ReactContextBaseJavaModule implements TransportListener {
+    // MARK: private variables
+    private boolean transportConfigured = false;
+    private Node node;
+    private Transport transport;
+    private Vector<Link> links;
+    private Activity activity;
+
+    // MARK: ReactContextBaseJavaModule
+    public NetworkManager(ReactApplicationContext reactContext, Activity inActivity) {
+        super(reactContext);
+        this.activity = inActivity;
+    }
+
+    public String getName() {
+        return "NetworkManager";
+    }
+
+    private void initTransport(String kind) {
+        if(transportConfigured){
+            return;
+        }
+        long nodeId = 0;
+        while(nodeId == 0) {
+            nodeId = new Random().nextLong();
+        }
+        EnumSet<TransportKind> kinds = null;
+        if(nodeId < 0) {
+            nodeId = -nodeId;
+        }
+        if(kind == "WIFI") {
+            EnumSet.of(TransportKind.WIFI);
+
+        } else if(kind == "BT") {
+            kinds = EnumSet.of(TransportKind.BLUETOOTH);
+
+        } else {
+            kinds = EnumSet.of(TransportKind.WIFI, TransportKind.BLUETOOTH);
+        }
+        this.transport = Underdark.configureTransport(
+                234235,
+                nodeId,
+                this,
+                null,
+                activity.getApplicationContext(),
+                kinds
+        );
+        this.transportConfigured = true;
+        transport.start();
+    }
+    private void stopTransport() {
+        transport.stop();
+    }
+    // MARK: React Methods
+    @ReactMethod
+    public void advertise(String kind) {
+        initTransport(kind);
+    }
+    @ReactMethod
+    public void browse(String kind) {
+        initTransport(kind);
+    }
+    @ReactMethod
+    public void stopAdvertising() {
+        stopTransport();
+    }
+    @ReactMethod
+    public void stopBrowsing() {
+        stopTransport();
+    }
+    //MARK: TransportListener
+    @Override
+    public void transportNeedsActivity(Transport transport, ActivityCallback callback) {
+    }
+    @Override
+    public void transportLinkConnected(Transport transport, Link link) {
+        this.links.add(link);
+        System.out.println("Link Connected");
+    }
+
+    @Override
+    public void transportLinkDisconnected(Transport transport, Link link) {
+        for(int i = 0; i < this.links.size(); ++i)
+            if (link.getNodeId() == this.links.elementAt(i).getNodeId()) {
+                this.links.removeElementAt(i);
+                return;
+            }
+        System.out.println("Link Disconnected");
+    }
+
+    @Override
+    public void transportLinkDidReceiveFrame(Transport transport, Link link, byte[] frameData) {
+        System.out.println("Recieved frame");
+    }
+}
--- index.android.js
+++ index.android.js
@@ -1,53 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-var NetworkManager = NativeModules.NetworkManager;
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  NativeModules,
 } from 'react-native';
+var NetworkManager = require('./NetworkManager.js')
 
 class RCTUnderdark extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      browsing: false,
+      advertising: false,
+    }
+    this.toggleAdvertise = this.toggleAdvertise.bind(this)
+    this.toggleBrowse = this.toggleBrowse.bind(this)
+    this.getButtonStyle = this.getButtonStyle.bind(this)
+  }
+  toggleBrowse() {
+    if(this.state.browsing) {
+      NetworkManager.stopBrowsing()
+    } else {
+      NetworkManager.browse("WIFI-BT")
+    }
+    this.setState({
+      browsing: !this.state.browsing
+    })
+  }
+
+  toggleAdvertise() {
+    if(this.state.advertising) {
+      NetworkManager.stopAdvertising()
+    } else {
+      NetworkManager.advertise("WIFI-BT")
+    }
+    this.setState({
+      advertising: !this.state.advertising
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <TouchableOpacity onPress={()=>{
+          this.toggleAdvertise()
+        }}>
+          <View style={this.getButtonStyle(this.state.advertising)}>
+            <Text style={styles.scanText}>ADVERTISE</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{
+          this.toggleBrowse()
+        }}>
+          <View style={this.getButtonStyle(this.state.browsing)}>
+            <Text style={styles.scanText}>BROWSE</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
+  getButtonStyle(on) {
+    if(on) {
+      return {
+        backgroundColor: "black",
+        height: 35,
+        width: 100,
+        marginBottom: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+    } else {
+      return {
+        backgroundColor: "gray",
+        height: 35,
+        width: 100,
+        marginBottom: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+    }
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5F5F5',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  onButton: {
+    backgroundColor: "black",
+    height: 35,
+    width: 100,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#FF00',
-    marginBottom: 5,
+  offButton: {
+    backgroundColor: "gray",
+    height: 35,
+    width: 100,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
+  scanText: {
+    color: "white",
+    textAlign: "center",
+  }
 });
 
 AppRegistry.registerComponent('RCTUnderdark', () => RCTUnderdark);
